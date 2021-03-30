9b16acc037b36c2a1d9a09991764fc5114aea008
root <root@.localdomain>
2021-03-30 23:11:41 +0900

    ex08 done with integer array. Well working :D

--- ex08/ft_print_combn.c
+++ ex08/ft_print_combn.c
@@ -6,84 +6,142 @@
 /*   By: sohan <sohan@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 19:28:54 by sohan             #+#    #+#             */
-/*   Updated: 2021/03/30 06:01:23 by sohan            ###   ########.fr       */
+/*   Updated: 2021/03/30 23:09:16 by sohan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-void	display_combn(char comb[], int n)
+int test = 0;
+void	initialize_combn(int *icomb, int count, int n);
+void	display_combn(int *dcomb, int n);
+void	cp_combn(int *tcomb, int *dcomb, int n);
+int	cnt_combn(int *comb, int n);
+
+void	initialize_from_orig(int *icomb, int count, int n)
 {
-	int end;
-	end = 9;
-	
-	while (comb[n-1] <= end + 48)
+	int j;
+
+	j = 0;
+	while (j <= count)
 	{
-		write(1, comb, n);
-		printf(" ");
-		comb[n-1] = comb[n-1] + 1;
+		icomb[n - 1 - j] = icomb[n - 1 - j] + 1;
+		j++;
 	}
-	return ;
 }
 
-//void	make_ascending(char comb[], int n, int i)
-//{
-//		display_combn(comb, n, i);
-//}
-void	ft_print_combn(int n)
-{	
-	char comb[n];
+void	initialize_from_comb(int *icomb, int count, int n)
+{
 	int i;
-	int j;
-	int k;
+
+	i = 1;
+	icomb[n-count-1] = icomb[n - count -1] + 1;
+	while (i <= count)
+	{
+		icomb[n - count - 1 + i] = icomb[n - count - 1] + i;
+		i++;
+	}
+}
+
+void	add_combn(int *acomb, int n)
+{
+	int ocomb[n];
+	int count;
+
+	cp_combn(ocomb , acomb, n);
+	while (acomb[n - 1] != 9)	
+	{
+		acomb[n - 1] = acomb[n - 1] + 1;
+		display_combn(acomb, n);
+	}
+	//printf("I'm Out!\n");
+	count = cnt_combn(acomb, n);
+	if (count == n - 1 || n == 1)
+	{
+		return ;
+	}
+	initialize_from_comb(acomb, count, n);
+	display_combn(acomb, n);
+}
+void	display_combn(int *dcomb, int n)
+{	
+	int i;
+
+	i = 0;
+	//printf("%d ",test+1);	
+	while (i < n)
+	{
+		printf("%d ",dcomb[i]);
+		i++;
+	}
+	printf("\n");
+	test++;
+}
+
+void	cp_combn(int *tcomb, int *dcomb, int n)
+{
+	int i;
+	
+	i = 0;
+	while(i < n)
+	{
+		tcomb[i] = dcomb[i];
+		i++;
+	}
+}
+
+int	cnt_combn(int *comb, int n)
+{	
+	int index;
+	int count;
+
+	count = 0;
+	index = 1;
+	while (index <= n)
+	{	
+		if(comb[n-index] == 10 - index)
+		{
+			++count;
+		}
+		index++;
+	}
+	return (count);
+}
+
+void	ft_print_combn(int n)
+{
+	int comb[n];
+	int orig[n];
+	int count;
 	int init;
 
 	init = n;
-	i = 0;
-	j = n - 1;
-	k= 1;
+	count = 0;
 	while (n--)
 	{
-		comb[n] = n + 48; //initialize
+		comb[n] = n;
+		orig[n] = comb[n];
 	}
 	n = init;
-	while (i<100)
-	{
-		display_combn(comb,init);
-		//write(1, comb, n);
-		while (comb[n-1] != 48)
-		{
-			comb[n-1] = comb[n-1] + 1;
-			n--;
+	display_combn(comb,n);
+	while (comb[0] < 10 - n)
+	{	
+		add_combn(comb, n);
+		count = cnt_combn(comb, n);
+		if (count == n - 1)	
+		{	
+	 		initialize_from_orig(orig, count, n);
+			cp_combn(comb, orig, n);
+			display_combn(comb, n);
+			count = 0;
 		}
-		n = init;
-		i++;
-	} 
-	
-	//while (comb[0] <= 10 - n + 48)
-	//{
-	//	display_combn(comb , comb[i - 1], end, n);
-	//	comb[i - 2] = comb [i - 2] + 1;
-	//	comb[i - 1] = comb[i - 1] + 1;
-	//	i--;
-	//	end--;
-		//printf("Is running\n");
-	//	write(1, comb, n);
-	//	printf("\n");
-		//init--;
-	//}
-	//for (int i=0;i<init;i++)
-	//{
-	//	printf("%d ",comb[i]);
-	//}
-	//printf("\n");
-
+	}
 	return ;
-
 }
 int	main(void)
 {
-	ft_print_combn(3);
+	ft_print_combn(9);
+	printf("number of combination:%d",test);
 	return 0;
 }
